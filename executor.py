6b5e8eb5c5a6f0e13fa134cfb1467e8e613c3519
Martin Matak <martin.matak@gmail.com>
2019-02-12 18:48:14 +0100

    documentation of executor and constant for max num of concurrent processes

--- executor.py
+++ executor.py
@@ -3,10 +3,19 @@ import subprocess
 import os
 import sys
 
+'''
+For every image, an adversarial sample is crated for every quantization level (2, 4, .. 64).
+Creation of an adversarial sample is isolated in a separate process.
+It follows that for every image, there will be 6 different processes.
+But for creation of an adversarial sample for the next image, it is not necessary to wait for the previous image to finish.
+So the creation of adversarial samples for the next image starts in parallel with the creation of adv samples for the previous image and so on.
+'''
 
 quantization=[2,4,8,16,32,64]
 
-concurrentProcesses=len(quantization)
+# controls total number of concurrent process that is run, if set too high, not-enough-memory exception could occur
+MAX_NUMBER_OF_CONCURRENT_PROCESSES = 60
+
 print(sys.argv[1])
 seed=int(sys.argv[1])
 startPoint=int(sys.argv[2])
@@ -24,6 +33,6 @@ for sourceImage in range(startPoint,lenTest):
 		exe=shlex.split(exe)
 		p=subprocess.Popen(exe,shell=False)
 		processes[p.pid]=1	#.append(p)
-		if len(processes.keys())>=concurrentProcesses:
+		if len(processes.keys())>=MAX_NUMBER_OF_CONCURRENT_PROCESSES:
 			(pid,exitstat) = os.wait()
 			del processes[pid]
