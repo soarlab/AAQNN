6185f9ab559539d1b9f2916d6aec62b124cdca33
Martin Matak <martin.matak@gmail.com>
2019-03-01 16:39:27 +0100

    instead of running N processes, create a pool of them based on number of cores

--- executor.py
+++ executor.py
@@ -2,6 +2,8 @@ import shlex
 import subprocess
 import os
 import sys
+import time
+import multiprocessing
 
 '''
 For every image, an adversarial sample is crated for every quantization level (2, 4, .. 64).
@@ -11,28 +13,38 @@ But for creation of an adversarial sample for the next image, it is not necessar
 So the creation of adversarial samples for the next image starts in parallel with the creation of adv samples for the previous image and so on.
 '''
 
-quantization=[2,4,8,16,32,64]
 
-# controls total number of concurrent process that is run, if set too high, not-enough-memory exception could occur
-MAX_NUMBER_OF_CONCURRENT_PROCESSES = 60
+def process_image(cmd):
+	cmd_original = cmd
+	print("Starting: " + str(cmd_original))
+	cmd = shlex.split(cmd)
+	r = subprocess.Popen(cmd, shell=False)
+	r.wait()
+	print("Finished: " + str(cmd_original))
 
-print(sys.argv[1])
-seed=int(sys.argv[1])
-startPoint=int(sys.argv[2])
-lenTest=int(sys.argv[3])
-dataset=str(sys.argv[4])
 
-processes={}
 
-for sourceImage in range(startPoint,lenTest):
-	print(sourceImage)
-	index0=sourceImage
-	for q in quantization:
-		exe="python main.py "+dataset+" ub cooperative "+str(index0)+" L2 10 1 "+str(q)+" "+str(q)+" "+str(seed)
-		print(exe)
-		exe=shlex.split(exe)
-		p=subprocess.Popen(exe,shell=False)
-		processes[p.pid]=1	#.append(p)
-		if len(processes.keys())>=MAX_NUMBER_OF_CONCURRENT_PROCESSES:
-			(pid,exitstat) = os.wait()
-			del processes[pid]
+def main():
+	pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+	print("pool size: ")
+	quantization = [2, 4, 8, 16, 32, 64]
+
+	print(sys.argv[1])
+	seed = int(sys.argv[1])
+	startPoint = int(sys.argv[2])
+	lenTest = int(sys.argv[3])
+	dataset = str(sys.argv[4])
+
+	for sourceImage in range(startPoint, lenTest):
+		print(sourceImage)
+		index0 = sourceImage
+		for q in quantization:
+			exe = "python main.py " + dataset + " ub cooperative "+str(index0)+" L2 10 1 "+str(q)+" "+str(q)+" "+str(seed)
+			pool.apply_async(process_image, [exe])
+
+	pool.close()
+	pool.join()
+
+
+if __name__=="__main__":
+	main()
