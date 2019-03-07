2587b12eefc1ebb253c19d01e99b95831718fdfa
Martin Matak <martin.matak@gmail.com>
2019-03-07 12:25:16 +0100

    add validation of arguments, move execution to main.py

--- executor.py
+++ executor.py
@@ -1,30 +1,10 @@
 from __future__ import print_function
-from keras import backend as K
 import sys
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-
-from NeuralNetwork import *
 import multiprocessing
 from DataCollection import *
-from upperbound import upperbound
-from lowerbound import lowerbound
+from main import process_image
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-#GPU SETTINGS
-# def CudaMemorySettings():
-#     config = tf.ConfigProto()
-#     config.gpu_options.allow_growth = True
-#     config.gpu_options.per_process_gpu_memory_fraction = 0.1
-#     config.gpu_options.visible_device_list = "0"
-#     set_session(tf.Session(config=config))
-
-
-def CpuMemorySettings():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-    session_conf = tf.ConfigProto(device_count={'GPU' : 0}, allow_soft_placement=True, log_device_placement=False)
-    set_session(tf.Session(config=session_conf))
 
 '''
 For every image, an adversarial sample is crated for every quantization level (2, 4, .. 64).
@@ -33,37 +13,6 @@ It follows that for every image, there will be 6 different processes.
 But for creation of an adversarial sample for the next image, it is not necessary to wait for the previous image to finish.
 So the creation of adversarial samples for the next image starts in parallel with the creation of adv samples for the previous image and so on.
 '''
-
-
-def process_image(dataset_name, bound, game_type, image_index, distance_measure, distance, tau, wbits, abits, seed):
-    # CudaMemorySettings()
-    CpuMemorySettings()
-
-    eta = (distance_measure, distance)
-    file_name = "seed_" + str(seed) + "_" + str(dataset_name) + "_" + str(image_index) + "_Wbits" + str(wbits) + "Abits" + str(abits) + ".txt"
-
-    # calling algorithms
-    dc = DataCollection("%s_%s_%s_%s_%s_%s_%s" % (dataset_name, bound, tau, game_type, image_index, eta[0], eta[1]))
-    dc.initialiseIndex(image_index)
-
-    if bound == 'ub':
-        (elapsedTime, newConfident, percent, l2dist, l1dist, l0dist, maxFeatures) = \
-            (upperbound(dataset_name, bound, tau, game_type, image_index, eta, wbits, abits, file_name, seed))
-
-        dc.addRunningTime(elapsedTime)
-        dc.addConfidence(newConfident)
-        dc.addManipulationPercentage(percent)
-        dc.addl2Distance(l2dist)
-        dc.addl1Distance(l1dist)
-        dc.addl0Distance(l0dist)
-        dc.addMaxFeatures(maxFeatures)
-    elif bound == 'lb':
-        lowerbound(dataset_name, image_index, game_type, eta, tau, wbits, abits)
-
-    dc.provideDetails()
-    dc.summarise()
-    dc.close()
-    K.clear_session()
 
 
 def main():
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from __future__ import print_function
-from keras import backend as K
-import sys
-import os
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
 from NeuralNetwork import *
-from DataSet import *
 from DataCollection import *
 from upperbound import upperbound
 from lowerbound import lowerbound
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-network_type='full-qnn'
-finetune=True
+network_type = 'full-qnn'
+finetune = True
 
-#GPU SETTINGS#
+
+# GPU SETTINGS#
 def CudaMemorySettings():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -24,106 +21,123 @@ def CudaMemorySettings():
     config.gpu_options.visible_device_list = "0"
     set_session(tf.Session(config=config))
 
+
 def CpuMemorySettings():
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     session_conf = tf.ConfigProto(
-    device_count={'GPU' : 0},
-    allow_soft_placement=True,
-    log_device_placement=False)
+        device_count={'GPU': 0},
+        allow_soft_placement=True,
+        log_device_placement=False
+    )
     set_session(tf.Session(config=session_conf))
 
-print ("OK")
-if len(sys.argv) == 11:
-	if sys.argv[1] == 'mnist' or sys.argv[1] == 'cifar10' or sys.argv[1] == 'fashion':
-			dataSetName = sys.argv[1]
-	else:
-			print("please specify as the 1st argument the dataset: mnist or cifar10 or gtsrb")
-			exit(1)
-	if sys.argv[2] == 'ub' or sys.argv[2] == 'lb':
-			bound = sys.argv[2]
-	else:
-			print("please specify as the 2nd argument the bound: ub or lb")
-			exit(1)
-	
-	if sys.argv[3] == 'cooperative' or sys.argv[3] == 'competitive':
-			gameType = sys.argv[3]
-	else:
-			print("please specify as the 3nd argument the game mode: cooperative or competitive")
-			exit(1)
-	
-	if isinstance(int(sys.argv[4]), int):
-			image_index = int(sys.argv[4])
-	else:
-			print("please specify as the 4th argument the index of the image: [int]")
-			exit(1)
-	
-	if sys.argv[5] == 'L0' or sys.argv[5] == 'L1' or sys.argv[5] == 'L2':
-			distanceMeasure = sys.argv[5]
-	else:
-			print("please specify as the 5th argument the distance measure: L0, L1, or L2")
-			exit(1)
-	
-	if isinstance(float(sys.argv[6]), float):
-			distance = float(sys.argv[6])
-	else:
-			print("please specify as the 6th argument the distance: [int/float]")
-			exit(1)
-	eta = (distanceMeasure, distance)
-	
-	if isinstance(float(sys.argv[7]), float):
-			tau = float(sys.argv[7])
-	else:
-			print("please specify as the 7th argument the tau: [int/float]")
-			exit(1)
-			
-	if isinstance(int(sys.argv[8]),int):
-			wbits= int(sys.argv[8])
-	else:
-			print("please specify as the 8th argument the n of weights bits: [int/float]")
-			exit(1)
-			
-	if isinstance(int(sys.argv[9]),int):
-			abits= int(sys.argv[9])
-	else:
-			print("please specify as the 8th argument the n of activ. bits: [int/float]")
-			exit(1)
-			
-	seed=int(sys.argv[10])
-	print ("Seed:"+str(seed))
-else:
-    print ("Incorrect parameters")
-    exit(1)
- 
-#CudaMemorySettings()
-CpuMemorySettings()
 
-nameFile="seed_"+str(seed)+"_"+str(dataSetName)+"_"+str(image_index)+"_Wbits"+str(wbits)+"Abits"+str(abits)+".txt"
+def validate_arguments(dataset_name,
+                       bound,
+                       game_type,
+                       image_index,
+                       distance_measure,
+                       distance,
+                       tau,
+                       wbits,
+                       abits,
+                       seed):
+    if dataset_name != 'mnist' and dataset_name != 'cifar10' and dataset_name != 'fashion':
+        print("please specify the dataset: mnist or cifar10 or fashion")
+        exit(1)
 
-# calling algorithms
-dc = DataCollection("%s_%s_%s_%s_%s_%s_%s" % (dataSetName, bound, tau, gameType, image_index, eta[0], eta[1]))
-dc.initialiseIndex(image_index)
+    if bound != 'ub' and bound != 'lb':
+        print("please specify the bound as : ub or lb")
+        exit(1)
 
-if bound == 'ub':
-    (elapsedTime, newConfident, percent, l2dist, l1dist, l0dist, maxFeatures) = (
-        upperbound(dataSetName, bound, tau, gameType, image_index, eta, wbits, abits,nameFile,seed))
+    if game_type != 'cooperative' and game_type != 'competitive':
+        print("please specify the game type as: cooperative or competitive")
+        exit(1)
 
-    dc.addRunningTime(elapsedTime)
-    dc.addConfidence(newConfident)
-    dc.addManipulationPercentage(percent)
-    dc.addl2Distance(l2dist)
-    dc.addl1Distance(l1dist)
-    dc.addl0Distance(l0dist)
-    dc.addMaxFeatures(maxFeatures)
+    if not isinstance(image_index, int):
+        print("please specify the index of the image as type [int]")
+        exit(1)
 
-elif bound == 'lb':
-    lowerbound(dataSetName, image_index, gameType, eta, tau, wbits, abits)
+    if distance_measure != 'L0' and distance_measure != 'L1' and distance_measure != 'L2':
+        print("please specify the distance measure as: L0, L1, or L2")
+        exit(1)
 
-else:
-    print("lower bound algorithm is developing...")
-    exit
+    if not (isinstance(distance, float) or isinstance(distance, int)):
+        print("please specify the distance as type [int/float]")
+        exit(1)
 
-dc.provideDetails()
-dc.summarise()
-dc.close()
+    if not (isinstance(tau, float) or isinstance(tau, int)):
+        print("please specify the tau as type [int/float]")
+        exit(1)
 
-K.clear_session()
+    if not isinstance(wbits, int):
+        print("please specify the wbits as type [int]")
+        exit(1)
+
+    if not isinstance(abits, int):
+        print("please specify the wbits as type [int]")
+        exit(1)
+
+    if not isinstance(seed, int):
+        print("please specify the seed as type [int]")
+        exit(1)
+
+    print("All arguments validated successfully")
+
+
+def process_image(dataset_name,
+                  bound,
+                  game_type,
+                  image_index,
+                  distance_measure,
+                  distance,
+                  tau,
+                  wbits,
+                  abits,
+                  seed):
+    validate_arguments(dataset_name, bound, game_type, image_index,
+                       distance_measure, distance, tau, wbits, abits, seed)
+    print("OK")
+
+    eta = (distance_measure, distance)
+
+    print("Seed:" + str(seed))
+
+    # CudaMemorySettings()
+    CpuMemorySettings()
+
+    file_name = "seed_" + str(seed) + \
+                "_" + str(dataset_name) + \
+                "_" + str(image_index) + \
+                "_Wbits" + str(wbits) + \
+                "Abits" + str(abits) + \
+                ".txt"
+
+    # calling algorithms
+    dc = DataCollection("%s_%s_%s_%s_%s_%s_%s" % (dataset_name, bound, tau, game_type, image_index, eta[0], eta[1]))
+    dc.initialiseIndex(image_index)
+
+    if bound == 'ub':
+        (elapsedTime, newConfident, percent, l2dist, l1dist, l0dist, maxFeatures) = (
+            upperbound(dataset_name, bound, tau, game_type, image_index, eta, wbits, abits, file_name, seed))
+
+        dc.addRunningTime(elapsedTime)
+        dc.addConfidence(newConfident)
+        dc.addManipulationPercentage(percent)
+        dc.addl2Distance(l2dist)
+        dc.addl1Distance(l1dist)
+        dc.addl0Distance(l0dist)
+        dc.addMaxFeatures(maxFeatures)
+
+    elif bound == 'lb':
+        lowerbound(dataset_name, image_index, game_type, eta, tau, wbits, abits)
+
+    else:
+        print("lower bound algorithm is developing...")
+        exit(1)
+
+    dc.provideDetails()
+    dc.summarise()
+    dc.close()
+
+    K.clear_session()
