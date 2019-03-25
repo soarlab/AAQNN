ee592b2f53f56eb717cf105645520bd4cd7137d7
Martin Matak <martin.matak@gmail.com>
2019-03-25 15:28:48 +0100

    add default yes

--- repo-init.sh
+++ repo-init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get install python3-pip
-sudo apt-get install virtualenv
+sudo apt-get --yes install python3-pip
+sudo apt-get --yes install virtualenv
 
 virtualenv -p python3 venv
 source venv/bin/activate
+yes | pip install -r requirements.txt
 
 git clone git://github.com/lisa-lab/pylearn2.git
 cd pylearn2/
